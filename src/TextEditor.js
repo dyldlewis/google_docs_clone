1771dbab7f83a2914ac4bc00082c50fc3f75d219
Dylan Lewis <dylanlewis@dylsmacbook.lan>
2021-06-08 12:20:24 -0700

    add more styling and toolbar options

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -3,6 +3,18 @@ import 'quill/dist/quill.snow.css'
 import { useCallback } from 'react'
 import "./styles.css"
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
+
 export default function TextEditor() {
   const wrapperRef = useCallback(wrapper => {
     if (wrapper == null) return
@@ -10,7 +22,7 @@ export default function TextEditor() {
     wrapper.innerHTML = ""
     const editor = document.createElement("div")
     wrapper.append(editor)
-    new Quill(editor, {theme: "snow"})
+    new Quill(editor, {theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS }})
   }, [])
   return <div className="container" ref={wrapperRef}></div>
 }
