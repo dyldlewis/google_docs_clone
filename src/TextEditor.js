98290d47a9bd49318524c88aee6782dc7b37b43e
Dylan Lewis <dylanlewis@dylsmacbook.lan>
2021-06-08 12:38:16 -0700

    add server and minimal socket.io

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,7 +1,8 @@
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import "./styles.css"
+import { io } from 'socket.io-client'
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +17,14 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+  useEffect(() => {
+    const socket = io("http://localhost:3001")
+
+    return () => {
+      socket.disconnect();
+    }
+  })
+
   const wrapperRef = useCallback(wrapper => {
     if (wrapper == null) return
 
