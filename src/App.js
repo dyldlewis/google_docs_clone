c60cfc41b3773277229b30ef26991688bf727573
Dylan Lewis <dylanlewis@dylsmacbook.lan>
2021-06-08 13:41:12 -0700

    add routing for unique page instances

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
 import TextEditor from './TextEditor.js'
-  
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom' 
+import { v4 as uuidV4 } from 'uuid'
+
 function App() {
-  return <TextEditor />
-  
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+
+        </Route>
+        <Route path="/documents/:id" exact>
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
