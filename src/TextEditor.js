0027bc3c50e0a3dfc5205ac57c783249483351e9
dyldlewis <dyldlewis@gmail.com>
2021-06-09 12:30:19 -0700

    minor functionallity change

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -5,6 +5,7 @@ import "./styles.css"
 import { io } from 'socket.io-client'
 import { useParams } from 'react-router-dom'
 
+const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -42,6 +43,20 @@ export default function TextEditor() {
     return () => {
       socket.off('recieve-changes', handler)
     }
+  }, [socket, quill])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const interval = setInterval(() => {
+      socket.emit('save-document', quill.getContents())
+    }, SAVE_INTERVAL_MS)
+
+    return () => {
+      clearInterval(interval)
+    } 
+     
+
   }, [socket, quill])
 
   useEffect(() => {
